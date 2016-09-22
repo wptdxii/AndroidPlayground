8aa631456403b3514e08ac03f9800bf14eaebfc3
wptdxii <wptdxii@gmail.com>
2016-09-22 18:41:53 +0800

    update

--- data/src/main/java/com/wptdxii/data/net/okhttp/OkClient.java
+++ data/src/main/java/com/wptdxii/data/net/okhttp/OkClient.java
@@ -7,6 +7,9 @@ import com.wptdxii.ext.Ext;
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+
 import okhttp3.Cache;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -42,6 +45,12 @@ public class OkClient {
                 .retryOnConnectionFailure(true)//失败重连
                 .readTimeout(TIMEOUT_READ, TimeUnit.SECONDS)
                 .connectTimeout(TIMEOUT_CONNECTION, TimeUnit.SECONDS)
+                .hostnameVerifier(new HostnameVerifier() {
+                    @Override
+                    public boolean verify(String hostname, SSLSession session) {
+                        return true;
+                    }
+                })
                 .cache(cache)//设置缓存目录
                 .build();
     }
