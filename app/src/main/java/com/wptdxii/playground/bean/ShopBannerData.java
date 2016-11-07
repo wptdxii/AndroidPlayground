b0ec82934aac8359f8da9f3939eba1ee1e7ebca0
wptdxii <wptdxii@gmail.com>
2016-11-07 22:42:18 +0800

    ddup

---
+++ app/src/main/java/com/wptdxii/playground/bean/ShopBannerData.java
@@ -0,0 +1,38 @@
+package com.wptdxii.playground.bean;
+
+/**
+ * Created by xionghu on 2016/8/18.
+ * Email：965705418@qq.com
+ */
+public class ShopBannerData {
+
+    private String id;
+    private String scene_name;
+    private String bannerUrl;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getScene_name() {
+        return scene_name;
+    }
+
+    public void setScene_name(String scene_name) {
+        this.scene_name = scene_name;
+    }
+
+    public String getBannerUrl() {
+        return bannerUrl;
+    }
+
+    public void setBannerUrl(String bannerUrl) {
+        this.bannerUrl = bannerUrl;
+    }
+
+
+}
---
+++ app/src/main/java/com/wptdxii/playground/bean/UserBean.java
@@ -0,0 +1,478 @@
+package com.wptdxii.playground.bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by bkyj-005 on 2016/10/18.
+ */
+
+public class UserBean implements Serializable {
+   private String add_time;
+   private String agent_id;
+   private String assessment;
+   private String assessment_expire;
+   private String avatar;
+   private String bank_name;
+   private String bank_no;
+   private String birthday;
+   private String ccyj_reference_flag;
+   private String ccyj_user_flag;
+   private String cert_a;
+   private String cert_b;
+   private String cert_num_isShowFlg;
+   private String checkin_time;
+   private String company;
+   private String company_name;
+   private String continue_checkin_times;
+   private String email;
+   private String expire;
+   private String expire_a;
+   private String expire_b;
+   private String good_count;
+   private String idno;
+   private String isShow_at_top;
+   private String isShow_in_expertlist;
+   private String is_del;
+   private String isshow_card;
+   private String lastlogin_date;
+   private String licence;
+   private String member_no;
+   private String mobile;
+   private String mobile_area;
+   private String mobile_num_short;
+   private String name;
+   private String nickname;
+   private String personal_context;
+   private String personal_specialty;
+   private String qq;
+   private String recomend_code;
+   private String referral_name;
+   private String referral_user_id;
+   private String referral_id;
+   private String region;
+   private String register_time;
+   private String setTop_time;
+   private String sex;
+   private String state;
+   private String token;
+   private String type;
+   private String user_id;
+   private String usrId;
+    private String user_id_encode;
+
+    public String getAdd_time() {
+        return add_time;
+    }
+
+    public void setAdd_time(String add_time) {
+        this.add_time = add_time;
+    }
+
+    public String getAgent_id() {
+        return agent_id;
+    }
+
+    public void setAgent_id(String agent_id) {
+        this.agent_id = agent_id;
+    }
+
+    public String getAssessment() {
+        return assessment;
+    }
+
+    public void setAssessment(String assessment) {
+        this.assessment = assessment;
+    }
+
+    public String getAssessment_expire() {
+        return assessment_expire;
+    }
+
+    public void setAssessment_expire(String assessment_expire) {
+        this.assessment_expire = assessment_expire;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getBank_name() {
+        return bank_name;
+    }
+
+    public void setBank_name(String bank_name) {
+        this.bank_name = bank_name;
+    }
+
+    public String getBank_no() {
+        return bank_no;
+    }
+
+    public void setBank_no(String bank_no) {
+        this.bank_no = bank_no;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getCcyj_reference_flag() {
+        return ccyj_reference_flag;
+    }
+
+    public void setCcyj_reference_flag(String ccyj_reference_flag) {
+        this.ccyj_reference_flag = ccyj_reference_flag;
+    }
+
+    public String getCcyj_user_flag() {
+        return ccyj_user_flag;
+    }
+
+    public void setCcyj_user_flag(String ccyj_user_flag) {
+        this.ccyj_user_flag = ccyj_user_flag;
+    }
+
+    public String getCert_a() {
+        return cert_a;
+    }
+
+    public void setCert_a(String cert_a) {
+        this.cert_a = cert_a;
+    }
+
+    public String getCert_b() {
+        return cert_b;
+    }
+
+    public void setCert_b(String cert_b) {
+        this.cert_b = cert_b;
+    }
+
+    public String getCert_num_isShowFlg() {
+        return cert_num_isShowFlg;
+    }
+
+    public void setCert_num_isShowFlg(String cert_num_isShowFlg) {
+        this.cert_num_isShowFlg = cert_num_isShowFlg;
+    }
+
+    public String getCheckin_time() {
+        return checkin_time;
+    }
+
+    public void setCheckin_time(String checkin_time) {
+        this.checkin_time = checkin_time;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getCompany_name() {
+        return company_name;
+    }
+
+    public void setCompany_name(String company_name) {
+        this.company_name = company_name;
+    }
+
+    public String getContinue_checkin_times() {
+        return continue_checkin_times;
+    }
+
+    public void setContinue_checkin_times(String continue_checkin_times) {
+        this.continue_checkin_times = continue_checkin_times;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getExpire() {
+        return expire;
+    }
+
+    public void setExpire(String expire) {
+        this.expire = expire;
+    }
+
+    public String getExpire_a() {
+        return expire_a;
+    }
+
+    public void setExpire_a(String expire_a) {
+        this.expire_a = expire_a;
+    }
+
+    public String getExpire_b() {
+        return expire_b;
+    }
+
+    public void setExpire_b(String expire_b) {
+        this.expire_b = expire_b;
+    }
+
+    public String getGood_count() {
+        return good_count;
+    }
+
+    public void setGood_count(String good_count) {
+        this.good_count = good_count;
+    }
+
+    public String getIdno() {
+        return idno;
+    }
+
+    public void setIdno(String idno) {
+        this.idno = idno;
+    }
+
+    public String getIsShow_at_top() {
+        return isShow_at_top;
+    }
+
+    public void setIsShow_at_top(String isShow_at_top) {
+        this.isShow_at_top = isShow_at_top;
+    }
+
+    public String getIsShow_in_expertlist() {
+        return isShow_in_expertlist;
+    }
+
+    public void setIsShow_in_expertlist(String isShow_in_expertlist) {
+        this.isShow_in_expertlist = isShow_in_expertlist;
+    }
+
+    public String getIs_del() {
+        return is_del;
+    }
+
+    public void setIs_del(String is_del) {
+        this.is_del = is_del;
+    }
+
+    public String getIsshow_card() {
+        return isshow_card;
+    }
+
+    public void setIsshow_card(String isshow_card) {
+        this.isshow_card = isshow_card;
+    }
+
+    public String getLastlogin_date() {
+        return lastlogin_date;
+    }
+
+    public void setLastlogin_date(String lastlogin_date) {
+        this.lastlogin_date = lastlogin_date;
+    }
+
+    public String getLicence() {
+        return licence;
+    }
+
+    public void setLicence(String licence) {
+        this.licence = licence;
+    }
+
+    public String getMember_no() {
+        return member_no;
+    }
+
+    public void setMember_no(String member_no) {
+        this.member_no = member_no;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getMobile_area() {
+        return mobile_area;
+    }
+
+    public void setMobile_area(String mobile_area) {
+        this.mobile_area = mobile_area;
+    }
+
+    public String getMobile_num_short() {
+        return mobile_num_short;
+    }
+
+    public void setMobile_num_short(String mobile_num_short) {
+        this.mobile_num_short = mobile_num_short;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPersonal_context() {
+        return personal_context;
+    }
+
+    public void setPersonal_context(String personal_context) {
+        this.personal_context = personal_context;
+    }
+
+    public String getPersonal_specialty() {
+        return personal_specialty;
+    }
+
+    public void setPersonal_specialty(String personal_specialty) {
+        this.personal_specialty = personal_specialty;
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq;
+    }
+
+    public String getRecomend_code() {
+        return recomend_code;
+    }
+
+    public void setRecomend_code(String recomend_code) {
+        this.recomend_code = recomend_code;
+    }
+
+    public String getReferral_name() {
+        return referral_name;
+    }
+
+    public void setReferral_name(String referral_name) {
+        this.referral_name = referral_name;
+    }
+
+    public String getReferral_user_id() {
+        return referral_user_id;
+    }
+
+    public void setReferral_user_id(String referral_user_id) {
+        this.referral_user_id = referral_user_id;
+    }
+
+    public String getReferral_id() {
+        return referral_id;
+    }
+
+    public void setReferral_id(String referral_id) {
+        this.referral_id = referral_id;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getRegister_time() {
+        return register_time;
+    }
+
+    public void setRegister_time(String register_time) {
+        this.register_time = register_time;
+    }
+
+    public String getSetTop_time() {
+        return setTop_time;
+    }
+
+    public void setSetTop_time(String setTop_time) {
+        this.setTop_time = setTop_time;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsrId() {
+        return usrId;
+    }
+
+    public void setUsrId(String usrId) {
+        this.usrId = usrId;
+    }
+
+    public String getUser_id_encode() {
+        return user_id_encode;
+    }
+
+    public void setUser_id_encode(String user_id_encode) {
+        this.user_id_encode = user_id_encode;
+    }
+}
