651171331947c2200baca0c74f9930324fe4eaac
wptdxii <wptdxii@gmail.com>
2017-11-02 08:15:48 +0800

    update

--- app/src/main/java/com/wptdxii/playground/ui/provider/MessageActionProvider.java
+++ app/src/main/java/com/wptdxii/playground/ui/provider/MessageActionProvider.java
@@ -1,7 +1,9 @@
 package com.wptdxii.playground.ui.provider;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.support.v4.view.ActionProvider;
+import android.view.LayoutInflater;
 import android.view.SubMenu;
 import android.view.View;
 
@@ -24,14 +26,17 @@ public class MessageActionProvider extends ActionProvider {
 
     @Override
     public View onCreateActionView() {
-        View view = View.inflate(mContext, R.layout.provider_message_action, null);
-        return null;
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+        @SuppressLint("InflateParams") View view = layoutInflater.inflate(R.layout.provider_message_action, null);
+        return view;
     }
 
     @Override
     public void onPrepareSubMenu(SubMenu subMenu) {
-        subMenu.add("Test1").setIcon(R.drawable.ic_arrow_back_white_24dp);
-        subMenu.add("Test2").setIcon(R.drawable.ic_logo);
+        super.onPrepareSubMenu(subMenu);
+        subMenu.clear();
+        subMenu.add("Submenu1").setIcon(R.drawable.ic_near_me_black_24dp);
+        subMenu.add("Submenu2").setIcon(R.drawable.ic_search_black_24dp);
     }
 
     @Override
