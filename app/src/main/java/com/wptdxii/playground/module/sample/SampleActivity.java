f5ebaa47f74e3843bdb3f12ea29f903262346786
wptdxii <wptdxii@gmail.com>
2017-12-01 18:31:33 +0800

    update

--- app/src/main/java/com/wptdxii/playground/module/sample/SampleActivity.java
+++ app/src/main/java/com/wptdxii/playground/module/sample/SampleActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.Toolbar;
 
 import com.wptdxii.playground.model.Component;
 import com.wptdxii.playground.module.base.BaseListActivity;
+import com.wptdxii.playground.module.sample.widget.ConstraintSampleActivity;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ public class SampleActivity extends BaseListActivity {
 
     @Override
     protected void onCreateComponentList(List<Component> list) {
+        list.add(new Component("ConstraintLayoutSample", ConstraintSampleActivity.class));
         list.add(new Component("ActionBarSample", ActionBarSampleActivity.class));
         list.add(new Component("ToolbarSample", ToolbarSampleActivity.class));
     }
---
+++ app/src/main/java/com/wptdxii/playground/module/sample/widget/ConstraintSampleActivity.java
@@ -0,0 +1,19 @@
+package com.wptdxii.playground.module.sample.widget;
+
+import android.os.Bundle;
+
+import com.wptdxii.playground.R;
+import com.wptdxii.uiframework.base.BaseActivity;
+
+public class ConstraintSampleActivity extends BaseActivity {
+
+    @Override
+    protected int onCreateContentView() {
+        return R.layout.activity_constraint_sample;
+    }
+
+    @Override
+    protected void onSetupContent(Bundle savedInstanceState) {
+
+    }
+}
